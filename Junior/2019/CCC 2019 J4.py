9388e7360fd7fcddb29292dd410d995754b96ac5
DanielYu2004 <DanielYu2004@users.noreply.github.com>
2019-12-09 22:49:54 -0500

    slightly better sol

--- Junior/2019/CCC 2019 J4.py
+++ Junior/2019/CCC 2019 J4.py
@@ -6,13 +6,13 @@ vert = flips.count('V') % 2
 if hori == 1 and vert == 1:
     print('4 3')
     print('2 1')
-if hori == 1 and vert == 0:
+elif hori == 1 and vert == 0:
     print('3 4')
     print('1 2')
-if hori == 0 and vert == 1:
+elif hori == 0 and vert == 1:
     print('2 1')
     print('4 3')
-if hori == 0 and vert == 0:
+else:
     print('1 2')
     print('3 4')
 #remember to try to make a one line sol
