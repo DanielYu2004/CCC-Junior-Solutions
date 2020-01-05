60a64ded2c0319a939dac5f40a8dfe8fc4f24290
DanielYu2004 <DanielYu2004@users.noreply.github.com>
2020-01-05 17:21:20 -0500

    Update CCC 2019 J5 V2.py

--- Junior/2019/CCC 2019 J5 V2.py
+++ Junior/2019/CCC 2019 J5 V2.py
@@ -9,30 +9,50 @@ def applyrules(string):
     for i in range(len(string)):
         if len(string) - i >= len(ruleone[0]):
             if string[i:len(ruleone[0])+i] == ruleone[0]:
-                output.append([string[0:i] + ruleone[1] + string[i+len(ruleone[0]):len(ruleone[0])], i, "ruleone"])
+                output.append([string[0:i] + ruleone[1] + string[i+len(ruleone[0]):], i, "ruleone"])
 
     for i in range(len(string)):
         if len(string) - i >= len(ruletwo[0]):
             if string[i:len(ruletwo[0])+i] == ruletwo[0]:
-                output.append([string[0:i] + ruletwo[1] + string[i+len(ruletwo[0]):len(ruletwo[0])], i, "ruletwo"])
+                output.append([string[0:i] + ruletwo[1] + string[i+len(ruletwo[0]):], i, "ruletwo"])
 
     for i in range(len(string)):
         if len(string) - i >= len(rulethree[0]):
             if string[i:len(rulethree[0])+i] == rulethree[0]:
-                output.append([string[0:i] + rulethree[1] + string[i+len(rulethree[0]):len(rulethree[0])], i, "rulethree"])
-    print(output)
+                output.append([string[0:i] + rulethree[1] + string[i+len(rulethree[0]):], i, "rulethree"])
+    return(output)
 
 
-#inputs = input().split()
-#steps = inputs[0]
-#initial = inputs[1]
-#end = inputs[2]
+inputs = input().split()
+steps = inputs[0]
+initial = inputs[1]
+end = inputs[2]
 
 
 class Node:
     def __init__(self, value, level):
         self.value = value
         self.level = level
+        self.children = {}
 
 
-applyrules("abc")
+    def determine_paths(self, nodes, level):
+        level +=1
+        queue = []
+        for node in nodes:
+            
+            for i in(applyrules(node.value)): #remember to consider repeat strings -> make a hashmap of already created nodes
+                print(i)
+                temp = Node(i[0], level)
+                node.children[temp.value] = [i[1], i[2]]
+                queue.append(temp)
+                print(temp.value)
+        self.determine_paths(queue, level)
+
+
+
+        return()
+
+root = Node(initial, 0)
+root.determine_paths([root], 0)
+#print(applyrules(initial))
