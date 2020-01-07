c42d03e3257a5b0483a9662c7e93002b394c664d
DanielYu2004 <DanielYu2004@users.noreply.github.com>
2020-01-06 23:29:47 -0500

    Update CCC 2017 J5.py

--- Junior/2017/CCC 2017 J5.py
+++ Junior/2017/CCC 2017 J5.py
@@ -19,18 +19,11 @@ for i in range(len(heights)):
     temparr = arr
     temparr = list(temparr)
     temparr.sort() #what the fukc why does removing .sort change anything what the fuck
-    #print(i)
     while (temparr):
-            #print(temparr)
-        #if ((heights[i] - temparr[0]) in temparr):
-            #if (temparr[1:].index(heights[i] - temparr[0])):
-        #if((heights[i]-temparr[0]) in temparr[1:]):
-        #if ((heights[i]-temparr[0] in temparr[1:])):
         if (heights[i] - temparr[0] in temparr[1:]):
             solheights[i] += 1    
             temparr.remove(heights[i] - temparr[0])
             del temparr[0]
-        #if len(temparr) > 0:
         else:
             del temparr[0]
 
@@ -42,6 +35,6 @@ occur = [0] * len(uniquesolheights)
 for i in range(len(uniquesolheights)):
     occur[i] = solheights.count(uniquesolheights[i])
 
-#print(heights)
-#print(solheights)
+print(heights)
+print(solheights)
 print(max(uniquesolheights), solheights.count(max(uniquesolheights)))
